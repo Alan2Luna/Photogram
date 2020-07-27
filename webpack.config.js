ef364a88d917b1618125a831d21a892cb80a7c18
Alan Luna <aklt1994@gmail.com>
2020-07-27 21:48:17 +0200

    Primer commit - Entorno de desarrollo para React con WebPack y Babel + styled-component

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filename: 'index.html'
+        })
+    ]
+}
